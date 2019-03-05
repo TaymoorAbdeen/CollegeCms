a502b9c675043b8b273b54ca387ce0be24cc3397
AreyXo <ari.muayad@gmail.com>
2019-03-05 10:26:46 +0300

    fixed wrong url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Laravel</title>
 	<link rel="stylesheet" href="{{asset('css/app.css')}}">
-	<link rel="stylesheet" href="{{asset('css/welcome.css')}}">
+  <link rel="stylesheet" href="/css/welcome.css">
   
    
 
